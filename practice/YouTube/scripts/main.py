c5ab8d5de2a9255246cb42c1df6e7d1965519de1
victor-von-pooh <m0b9d3t0f-novacane-iamrenegade@ezweb.ne.jp>
2025-07-25 14:15:02 +0900

    🎉Upload practice/YouTube/scripts/main

---
+++ practice/YouTube/scripts/main.py
@@ -0,0 +1,13 @@
+from utils import dl_youtube
+
+
+# configファイルを読み込む
+cfg_path = "cfg.txt"
+with open(cfg_path) as f:
+    urls = [s.rstrip() for s in f.readlines()]
+
+# ファイルを保存するデータパスを指定
+dir_path = "../downloaded"
+
+# 動画のダウンロード
+dl_youtube(urls, dir_path)
